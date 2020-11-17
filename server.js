87715abbde8520f643b16a32f507afcea2daf1fb
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2020-11-17 07:35:34 -0500

    express app starter files.

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+	res.status(200).json({
+		status: 'Congrats!',
+		message: 'Testing nodemon',
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
