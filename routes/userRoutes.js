ed5c4c7c052acabfabbb5c38be628fbb8751d300
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2020-11-20 10:05:56 -0500

    created endpoints for user

---
+++ routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
+router.post('/login', authController.login);
+router.post('/signup', authController.signup);
+
+router.route('/').get(userController.getAllUsers);
+
+module.exports = router;
