bd2dbfd497cd9424185790e7a1842fd8cd2866e7
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2020-11-23 07:36:39 -0500

    added handlers for general NodeJS errors.

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught Exception. Shutting down..');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './.env' });
 const app = require('./app');
 
@@ -20,6 +26,14 @@ mongoose
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandled rejection');
+  server.close(() => {
+    process.exit(1);
+  });
 });
