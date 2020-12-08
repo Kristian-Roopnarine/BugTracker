63519dbc602f44fe65825075dfcda093a78223ce
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2020-12-08 09:11:23 -0500

    added routes to reset and update password

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,12 +3,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
-
+// create middleware to protect routes and add user to req.body
 router.post('/login', authController.login);
 router.post('/signup', authController.signup);
 router.post('/forgotPassword', authController.forgotPassword);
-//router.patch('/resetPassword/:token', authController.resetPassword);
-
-router.route('/').get(userController.getAllUsers);
+router.patch('/resetPassword/:token', authController.resetPassword);
+router.patch('/updateMyPassword', authController.updatePassword);
 
 module.exports = router;
