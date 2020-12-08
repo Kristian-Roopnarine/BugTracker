377701087f156781cd2cc8c0eb75b0ce11ff6584
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2020-12-08 09:09:30 -0500

    moved login functionality to helper function and created resetPassword route.

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const AppError = require('./../utils/appError');
 const Users = require('../models/userSchema');
 const asyncCatchWrapper = require('./../utils/asyncCatchWrapper');
 const sendEmail = require('./../utils/email.js');
+const crypto = require('crypto');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -10,15 +11,21 @@ const signToken = (id) => {
   });
 };
 
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
 exports.signup = asyncCatchWrapper(async (req, res, next) => {
   const { body } = req;
   const data = await Users.create(body);
-  const token = signToken(data._id);
-  res.status(200).json({
-    status: 'created',
-    token,
-    data,
-  });
+  createSendToken(data, 201, res);
 });
 
 exports.login = asyncCatchWrapper(async (req, res, next) => {
@@ -32,11 +39,7 @@ exports.login = asyncCatchWrapper(async (req, res, next) => {
   if (!user || !(await user.isPasswordCorrect(password, user.password))) {
     return next(new AppError('Incorrect email or password', 404));
   }
-  const token = signToken(user._id);
-  res.status(200).json({
-    status: 'success',
-    token,
-  });
+  createSendToken(user, 200, res);
 });
 
 exports.forgotPassword = asyncCatchWrapper(async (req, res, next) => {
@@ -78,4 +81,44 @@ exports.forgotPassword = asyncCatchWrapper(async (req, res, next) => {
     status: 'success',
     message: 'Token sent to email',
   });
+});
+
+exports.resetPassword = asyncCatchWrapper(async (req, res, next) => {
+  // 1) Get user based on token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+  // 2) if token has not expired, and there is a user, set the new password
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400));
+  }
+  // 3) update changedPasswordAt property for the user
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+  // 4) log user in and send JWT
+  createSendToken(user, 200, res);
+});
+
+exports.updatePassword = asyncCatchWrapper(async (req, res, next) => {
+  // 1) Get user from collection
+  // create middleware to transform JWT to user id
+  const user = await User.findById(req.user.id).select('+password');
+  // 2) check if POSTed current pasword is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is wrong', 401));
+  }
+  // 3) if so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  // 4) log user in, send new JWT
+  createSendToken(user, 200, res);
 });
